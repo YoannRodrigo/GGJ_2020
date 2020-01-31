c04ce462c1f5c6395f1f276d7106c19d40043e89
Yoann <yomaisonneuve@gmail.com>
2020-01-31 21:32:49 +0100

    Player can rotate Object

---
+++ GGJ_2020/Assets/Scripts/Object/RotateObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] private Transform childTransform;
+    private void Update()
+    {
+        float x = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Horizontal");
+        
+        childTransform.Rotate(x, -y, 0,Space.World);
+    }
+}
