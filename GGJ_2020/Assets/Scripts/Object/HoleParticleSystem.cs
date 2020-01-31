9f589f87c88d63cadaf19f31cdd995130617ecf1
Roppii <ng.roche7@gmail.com>
2020-01-31 23:26:11 +0100

    Get random transform positions

---
+++ GGJ_2020/Assets/Scripts/Object/HoleParticleSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleParticleSystem : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private List<Transform> holeTransformList = new List<Transform>();
+    private HashSet<int> indexAlreadyChosen = new HashSet<int>();
+    public int maxHoles;
+    #endregion
+
+    private void Awake() {
+        for (int i = 0; i < maxHoles; i++) {
+            int randomChildIndex = UnityEngine.Random.Range(0, transform.childCount);
+            while (indexAlreadyChosen.Contains(randomChildIndex)) {
+                randomChildIndex = UnityEngine.Random.Range(0, transform.childCount);
+            }
+
+            indexAlreadyChosen.Add(randomChildIndex);
+            holeTransformList.Add(transform.GetChild(randomChildIndex));
+        }
+    }
+}
