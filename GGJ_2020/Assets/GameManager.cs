75954b6977d4c4268b34950e6514c3a7486b8255
Byzness <guillaume.briant@free.fr>
2020-02-01 02:45:45 +0100

    Switch between scenes using Fade

---
+++ GGJ_2020/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Animator fadeAnimator;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            StartCoroutine("SwitchScene");
+        }
+    }
+
+    IEnumerator SwitchScene() {
+        fadeAnimator.SetTrigger("triggerFade");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(1);
+    }
+}
