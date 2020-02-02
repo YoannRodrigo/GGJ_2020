07c40daffa39b071ac9f71a6ab93efa8c8f304f1
Yoann <yomaisonneuve@gmail.com>
2020-02-02 06:50:24 +0100

    Menu Navigation

---
+++ GGJ_2020/Assets/Scripts/UI/SelectControler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectControler : MonoBehaviour
+{
+    public List<Transform> framesPosition = new List<Transform>();
+    private int currentIndex;
+    public bool canbeMoved;
+    private bool isPositionReset = true;
+    
+    private void Update()
+    {
+        float x = Input.GetAxis("VerticalSelect");
+        if (x > 0.5f)
+        {
+            SelectUp();
+        }
+        else if (x < -0.5f)
+        {
+            SelectDown();
+        }
+        else
+        {
+            isPositionReset = true;
+        }
+            
+    }
+
+    private void SelectUp()
+    {
+        if (canbeMoved && isPositionReset)
+        {
+            isPositionReset = false;
+            currentIndex--;
+            if (currentIndex < 0)
+            {
+                currentIndex = framesPosition.Count - 1;
+            }
+
+            transform.position = framesPosition[currentIndex].position;
+        }
+    }
+
+    private void SelectDown()
+    {
+        if (canbeMoved && isPositionReset)
+        {
+            isPositionReset = false;
+            currentIndex++;
+            if (currentIndex >= framesPosition.Count)
+            {
+                currentIndex = 0;
+            }
+            transform.position = framesPosition[currentIndex].position;
+        }
+    }
+}
