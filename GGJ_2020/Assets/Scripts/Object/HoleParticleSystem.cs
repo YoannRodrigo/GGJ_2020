d1e1db50aa1a580a531c5565eafac7c012b7215d
Yoann <yomaisonneuve@gmail.com>
2020-02-02 05:52:13 +0100

    Fix Difficulty System

--- GGJ_2020/Assets/Scripts/Object/HoleParticleSystem.cs
+++ GGJ_2020/Assets/Scripts/Object/HoleParticleSystem.cs
@@ -2,18 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class HoleParticleSystem : MonoBehaviour
 {
     #region Variables
+    
+    public ObjectDifficulty difficulty;
     [SerializeField]
     private List<Transform> holeTransformList = new List<Transform>();
     private HashSet<int> indexAlreadyChosen = new HashSet<int>();
     [SerializeField] private GameObject objectToRemovePrefab;
-    public int maxHoles;
+    private int maxHoles;
     #endregion
 
     private void Awake() {
+        switch (difficulty)
+        {
+            case ObjectDifficulty.easy:
+                maxHoles = Random.Range(2, 4);
+                break;
+            case ObjectDifficulty.normal:
+                maxHoles = Random.Range(4, 6);
+                break;
+            case ObjectDifficulty.hard :
+                maxHoles = Random.Range(6, 8);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
         for (int i = 0; i < maxHoles; i++) {
             int randomChildIndex = UnityEngine.Random.Range(0, transform.childCount);
             while (indexAlreadyChosen.Contains(randomChildIndex)) {
